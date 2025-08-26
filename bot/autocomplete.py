08e35d60eaee31375dc5b5cf929f081c977536c6
Sébastien Doussang <sebastien.doussang@epitech.eu>
2025-08-26 10:51:10 +0200

    fix: typos + missing import

--- bot/autocomplete.py
+++ bot/autocomplete.py
@@ -1,4 +1,7 @@
 from bisect import bisect_left
+from discord import app_commands
+from bot.list import weapon_choices
+from func.auctions import weapons
 
 sorted_weapon_choices = sorted((display_name.lower(), display_name, api_name) for display_name, api_name in weapon_choices.items())
 sorted_weapons = sorted((weapon.lower(), weapon) for weapon in weapons)
@@ -14,9 +17,9 @@ def dichotomic_autocomplete(sorted_list, prefix, limit=25):
     return results
 
 async def add_autocomplete(interaction, current: str):
-    matches = autocomplete_dichotomie(sorted_weapon_choices, current)
+    matches = dichotomic_autocomplete(sorted_weapon_choices, current)
     return [app_commands.Choice(name=display_name, value=api_name) for _, display_name, api_name in matches]
 
 async def remove_autocomplete(interaction, current: str):
-    matches = autocomplete_dichotomie(sorted_weapons, current)
+    matches = dichotomic_autocomplete(sorted_weapons, current)
     return [app_commands.Choice(name=weapon_check[api_name_lower], value=weapon_name) for api_name_lower, weapon_name in matches]
